a94d302a1f45872299fc3e301323455a5101bde2
Student-KBH <student-kbh@Student-KBHs-MacBook-Pro.local>
2021-02-01 10:14:47 +0100

    fetch json

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 fetch("actors.json")
-. then(res=>res.json())
-.then(handleData);
+    .then(res => res.json())
+    .then(handleData);
 
-function HandleData(actors) {
-    forEach.actors ==> displayActors()
+function handleData(actors) {
+    console.log(actors)
+    actors.forEach(displayActor);
 }
 
-function displayActors(actor) {
+function displayActor(actor) {
+    console.log(actor)
+
+    const template = document.querySelector("template").content;
+
+    const copy = template.cloneNode(true);
+
+    copy.querySelector("h2").textContent = actor.fullname;
+    copy.querySelector("h3").textContent = actor.movie;
+
+    document.querySelector("body").appendChild(copy)
 
 }
+
+
